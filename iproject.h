2669f36d91d350aecb5523175c9cbb5b17904759
dvexer <bchystikov@luxoft.com>
2019-12-12 10:20:13 +0200

    using interface added

--- iproject.h
+++ iproject.h
@@ -3,7 +3,6 @@
 #include <string>
 
 namespace dev{
-// TODO: remove interface as not needed
 class IProject {
 public:
   virtual std::vector<double> process(const std::string & args) = 0;
--- main.cc
+++ main.cc
@@ -3,15 +3,16 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <memory>
 
 int main() {
     std::cout << "Enter input for calculator or 'q' for quit:" << std::endl;
     std::string input;
-    dev::Project proj;
+    const std::unique_ptr<dev::IProject> proj = std::make_unique<dev::Project>();
     do {
       getline(std::cin, input);
       try {
-          const auto & result = proj.process(input);
+          const auto & result = proj->process(input);
           if (result.size() == 1) {
             std::cout << result.back() << std::endl;
           } else if (result.size() > 1) {
@@ -19,6 +20,7 @@ int main() {
           } else {
             std::cerr << "Internal logic error, result is empty!" << std::endl;
           }
+          // TODO: propagate unparces symbol
       } catch ( const std::invalid_argument &) {
           std::cerr << "Exception: argument is nor number niether operator!" << std::endl;
       } catch (const std::exception & e) {
