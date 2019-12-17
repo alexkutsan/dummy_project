16776ec80a3ae3337913caa099e7afb068ef7255
dvexer <bchystikov@luxoft.com>
2019-12-17 13:30:43 +0200

    add some method descriptions

--- iproject.h
+++ iproject.h
@@ -5,6 +5,11 @@
 namespace dev{
 class IProject {
 public:
+    /**
+   * @brief process applies math operations from args to operands from arfs
+   * @param args list of ordered arguments and operations
+   * @return calculation result
+   */
   virtual std::vector<double> process(const std::string & args) = 0;
 };
 
--- project.h
+++ project.h
@@ -8,13 +8,38 @@ namespace dev {
 class Project : public IProject {
 friend class TestCalculatorProxy;
 public:
+/**
+* @brief process applies math operations from args to operands from arfs
+* @param args list of ordered arguments and operations
+* @return calculation result
+*/
   std::vector<double> process(const std::string &input) override;
 // TODO: make private and create object for testing private methods
 // private:
+  /**
+   * @brief isOperator check whether arg is operator
+   * @param arg is being checked whether it is operator
+   * @return whether arg is operator
+   */
   bool isOperator(const std::string & arg) const;
+  /**
+   * @brief applyOperation
+   * @param operation
+   * @param operands
+   */
   void applyOperation(const std::string & operation,
                       std::vector<double> &operands) const;
+  /**
+   * @brief parceInput
+   * @param input
+   * @return
+   */
   std::vector<std::string> parceInput(const std::string &input);
+  /**
+   * @brief processArguments
+   * @param args
+   * @return
+   */
   std::vector<double> processArguments(const std::vector<std::string> &args);
 };
 
