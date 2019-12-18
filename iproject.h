33002eea38d468e3aaeeca5d3adec1f5a6be17d9
KEK <ymamykin@luxoft.com>
2019-12-18 11:55:46 +0200

    Remove obsolete methods from IProject interface

--- iproject.h
+++ iproject.h
@@ -5,8 +5,6 @@ namespace dev{
 
 class IProject {
   virtual int run(const std::string& user_input) = 0;
-  virtual bool parse_string(const std::string& data) = 0;
-  virtual int calculate() = 0;
 };
 
 }
