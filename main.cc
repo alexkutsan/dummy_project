1d5ce0517ef356daf78a298f19730243c74880b2
dvexer <bchystikov@luxoft.com>
2019-12-19 11:00:10 +0200

    while loop removed

--- main.cc
+++ main.cc
@@ -8,26 +8,23 @@
 constexpr int valid_result_size = 1;
 
 int main() {
-    std::cout << "Enter input for calculator or 'q' for quit:" << std::endl;
     std::string input;
     const std::unique_ptr<dev::IProject> proj = std::make_unique<dev::Project>();
-    do {
-      getline(std::cin, input);
-      try {
-          const auto & result = proj->process(input);
-          if (result.size() == valid_result_size) {
-            std::cout << result.back() << std::endl;
-          } else if (result.size() > valid_result_size) {
-            std::cerr << "Invalid operations number!" << std::endl;
-          } else {
-            std::cerr << "Internal logic error, result is empty!" << std::endl;
-          }
-          // TODO: propagate unparces symbol
-      } catch ( const std::invalid_argument &) {
-          std::cerr << "Exception: argument is nor number niether operator!" << std::endl;
-      } catch (const std::exception & e) {
-          std::cerr << "Exception: " << e.what() << std::endl;
+    getline(std::cin, input);
+    try {
+      const auto & result = proj->process(input);
+      if (result.size() == valid_result_size) {
+        std::cout << result.back() << std::endl;
+      } else if (result.size() > valid_result_size) {
+        std::cerr << "Invalid operations number!" << std::endl;
+      } else {
+        std::cerr << "Internal logic error, result is empty!" << std::endl;
       }
-    } while (input != "q");
-  return 0;
+      // TODO: propagate unparces symbol
+    } catch ( const std::invalid_argument &) {
+      std::cerr << "Exception: argument is nor number niether operator!" << std::endl;
+    } catch (const std::exception & e) {
+      std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    return 0;
 }
