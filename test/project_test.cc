928ab63f70a773c97f59ac6c5ad30fe7d4ab71a8
Goran Shekerov <gshekerov@luxoft.com>
2020-07-17 10:55:36 +0200

    init calculator unit test
    TDD / extreame programing try

--- test/project_test.cc
+++ test/project_test.cc
@@ -15,5 +15,37 @@ TEST_F(ProjectTest, Run) {
   ASSERT_EQ(0, project_.run());
 }
 
+// TDD
+// basic design calc(std::sting) -> double
+
+TEST_F(ProjectTest, AddPositive) {
+    ASSERT_EQ(project_.calc("2 3 +"), 5);
+}
+
+TEST_F(ProjectTest, AddNegative) {
+    ASSERT_EQ(project_.calc("-2 -3 +"), -5);
+}
+
+TEST_F(ProjectTest, Substract) {
+    ASSERT_EQ(project_.calc("5 2 -"), 3);
+}
+
+TEST_F(ProjectTest, Multiply) {
+    ASSERT_EQ(project_.calc("3 7 *"), 21);
+}
+
+TEST_F(ProjectTest, Devide) {
+    ASSERT_EQ(project_.calc("21 7 /"), 3);
+}
+
+TEST_F(ProjectTest, MultiplyWithZero) {
+    ASSERT_EQ(project_.calc("21 0 *"), 0);
+}
+
+TEST_F(ProjectTest, DevideWithZero) {
+    ASSERT_EQ(project_.calc("21 0 /"), -1);
+}
+
+
 }  // namespace testing
 }  // namespace dev
