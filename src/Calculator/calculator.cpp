63aff0049e4c98d27e88f7498712536afdf9b2e3
Goran Shekerov <gshekerov@luxoft.com>
2020-07-20 11:26:53 +0200

    WIP: init Calculator

--- src/Calculator/calculator.cpp
+++ src/Calculator/calculator.cpp
@@ -1,5 +1,6 @@
 #include "include/calculator.h"
 #include <iostream>
+#include <algorithm>
 
 Calculator::Calculator()
 {
@@ -13,5 +14,50 @@ Calculator::~Calculator()
 
 double Calculator::calculate(std::string input)
 {
+    clear();
+    parseInput(input);
+
+    if (operators.at(0) == '+') {
+        return operands.at(0) + operands.at(1);
+    }
+
+    if (operators.at(0) == '-') {
+        return operands.at(0) - operands.at(1);
+    }
+
+    if (operators.at(0) == '*') {
+        return operands.at(0) * operands.at(1);
+    }
+
+    if (operators.at(0) == '/') {
+        return operands.at(0) / operands.at(1);
+    }
+
     return -1;
+}
+
+bool Calculator::parseInput(std::string input)
+{
+
+    for (auto o : input) {
+        if (isOperator(o)) {
+            operators.push_back(o);
+            return true;
+        }
+
+        operands.push_back(o);
+        return true;
+    }
+    return false;
+}
+
+void Calculator::clear()
+{
+    operands.clear();
+    operators.clear();
+}
+
+
+bool Calculator::isOperator(const char& oper) {
+    return std::find(availableOperators.begin(), availableOperators.end(), oper) != availableOperators.end();
 }
--- src/Calculator/include/calculator.h
+++ src/Calculator/include/calculator.h
@@ -1,11 +1,22 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 class Calculator{
+
 public:
     Calculator();
     ~Calculator();
 
     double calculate(std::string input);
+
+private:
+    bool parseInput(std::string input);
+    void clear();
+    bool isOperator(const char& oper);
+
+    std::vector<double> operands;
+    std::vector<char> operators;
+    const std::vector<char> availableOperators{'+', '-', '*', '/'};
 };
