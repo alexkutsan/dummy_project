d6c117b9676a2dc5151ea312b7ebbc0b5e38199b
Ravi Kandasamy Sundaram <rkandasamysundaram@luxoft.com>
2020-07-22 11:38:01 +0200

    refactor: create abstraction for tokens, operand & operator

--- calculator.cc
+++ calculator.cc
@@ -10,19 +10,34 @@
 
 namespace dev {
 
-bool Calculator::isOperator(const std::string& token) {
-    std::array<std::string, 4> operators = {"+", "-", "*", "/"};
-    return (std::find(operators.begin(), operators.end(), token) != operators.end());
-}
+IToken::IToken(const std::string& token):token_(token) {}
 
-double Calculator::toOperand(const std::string& token) {
+Operand::Operand(const std::string& token):IToken(token) {
     try {
-        return std::stod(token);
+        value_ = std::stod(token);
     } catch (const std::invalid_argument&) {
         throw InvalidOperandException();
     } catch (const std::out_of_range&) {
         throw OperandOutOfRangeException();
     }
+}
+
+double Operand::value() const {
+    return value_;
+}
+
+Operator::Operator(const std::string& token):IToken(token) {
+    // ToDo validate operator and raise exception
+    value_ = token[0];
+}
+
+char Operator::value() const {
+    return value_;
+}
+
+bool Calculator::isOperator(const std::string& token) {
+    std::array<std::string, 4> operators = {"+", "-", "*", "/"};
+    return (std::find(operators.begin(), operators.end(), token) != operators.end());
 }
 
 double Calculator::calc(const std::string& expression) {
@@ -33,12 +48,12 @@ double Calculator::calc(const std::string& expression) {
     std::stack<std::string> stkTokens;
     for (auto token : tokens) {
         if (isOperator(token)) {
-            double operand1 = toOperand(stkTokens.top());
+            Operand operand1 = Operand(stkTokens.top());
             stkTokens.pop();
-            double operand2 = toOperand(stkTokens.top());
+            Operand operand2 = Operand(stkTokens.top());
             stkTokens.pop();
 
-            stkTokens.push(std::to_string(evalOperation(operand1, operand2, token)));
+            stkTokens.push(std::to_string(evalOperation(operand1, operand2, Operator(token))));
         } else {
             stkTokens.push(token);
         }
@@ -47,21 +62,20 @@ double Calculator::calc(const std::string& expression) {
     return std::stod(stkTokens.top());
 }
 
-double Calculator::evalOperation(const double& operand1, const double& operand2, const std::string& operatr) {
-    char op = operatr[0];
-    switch (op)
+double Calculator::evalOperation(const Operand& operand1, const Operand& operand2, const Operator& operatr) {
+    switch (operatr.value())
     {
     case '+':
-        return (operand2 + operand1);
+        return (operand2.value() + operand1.value());
     case '-':
-        return (operand2 - operand1);
+        return (operand2.value() - operand1.value());
     case '*':
-        return (operand2 * operand1);
+        return (operand2.value() * operand1.value());
     case '/':
-        if (operand1 == 0) {
+        if (operand1.value() == 0) {
             throw DivideByZeroException();
         }
-        return (operand2 / operand1);
+        return (operand2.value() / operand1.value());
     default:
         throw InvalidOperatorException();
     }
--- calculator.h
+++ calculator.h
@@ -4,6 +4,29 @@
 
 namespace dev {
 
+class IToken {
+public:
+    IToken(const std::string& token);
+private:
+    const std::string& token_;
+};
+
+class Operand : public IToken {
+public:
+    Operand(const std::string& token);
+    double value() const;
+private:
+    double value_;
+};
+
+class Operator : public IToken {
+public:
+    Operator(const std::string& token);
+    char value() const;
+private:
+    char value_;
+};
+
 class Calculator : public ICalculator {
 public:
     Calculator() = default;
@@ -13,8 +36,7 @@ public:
 
 private:
     bool isOperator(const std::string& token);
-    double toOperand(const std::string& token);
-    double evalOperation(const double& operand1, const double& operand2, const std::string& operatr);
+    double evalOperation(const Operand& operand1, const Operand& operand2, const Operator& operatr);
 };
 
 }  // namespace dev
